b79f208bf1d74f206461bfce04022abf05293c00
HaithemGhattass <haithem.ghattas@esprit.tn>
2023-04-22 02:26:14 +0100

    chapter 1

---
+++ 06-animations/src/script.js
@@ -0,0 +1,54 @@
+import * as THREE from 'three'
+import gsap from 'gsap'
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Clock
+//const clock = new THREE.Clock()
+
+gsap.to(mesh.position,{duration : 1 ,delay : 1, x: 2})
+gsap.to(mesh.position,{duration : 1 ,delay : 2, x: 0})
+
+//Animation
+const tick = () => {
+
+   //clock 
+//    const elapsedtime = clock.getElapsedTime()
+//    console.log(elapsedtime)
+//     // update objects
+//     camera.position.y =  Math.sin(elapsedtime)
+//     camera.position.x =  Math.cos(elapsedtime)
+//     camera.lookAt(mesh.position)
+
+//     // Render 
+    renderer.render(scene, camera)
+
+     window.requestAnimationFrame(tick)
+}
+tick()
